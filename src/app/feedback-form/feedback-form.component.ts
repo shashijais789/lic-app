0eb56602526b10e54947442a7d1f26407302310c
Shashikant Jaiswal <shashikant.jaiswal@broadcom.com>
2018-10-01 02:18:21 +0530

    Submit feedback to DB fixed

--- src/app/feedback-form/feedback-form.component.ts
+++ src/app/feedback-form/feedback-form.component.ts
@@ -15,78 +15,66 @@ import {Observable} from 'rxjs/Observable';
 export class FeedbackFormComponent {
 
   constructor(private http: Http) { }
-  
-  
   apiRoot: string = "http://localhost:8090/api/fdi";  
   feedback = new Feedback(0, "Shashi", "Jais", false, true, 0, 0, "");
   feedbackList: any;
   submitted = false;
+  feedbackSubmitMsg: string = "";
+  feedbackSubmittedSinceServerStart = 0;
 
-  onSubmit() { 
+  
+/*
+  onSubmit1() { 
     console.log('submit called !!!!!');
     console.log(this.feedback);
     this.submitted = true; 
     //this.feedbackList =  this.http.get<Feedback[]>(this.apiRoot)
     console.log("Submit new Feedback!");
-    //let url = this.apiRoot}';
-    //console.log(url);
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     let opts = new RequestOptions();
     opts.headers = headers;
-    // this.http.post(url, this.feedback).subscribe(
-    //   res => console.log(res.json()),
-    //   msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-    // );
-    // this.http.get(url).subscribe(
-    //   res => console.log(console.log(res.text()),
-    //   msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-    // );
-    this.http.get(this.apiRoot).subscribe(res => console.log(res.json()));
+    this.http.get(this.apiRoot).subscribe(
+      res => { 
+        this.feedbackList = res.json(); 
+        console.log('Total Feedback registered: ' + this.feedbackList.length)},
+      msg => console.error('Error: ${msg.status} ${msg.statusText}')
+    );
   }
-
-
-  // onSubmit () {
-  //   this.feedbackList =  this.http.get<Feedback[]>(this.apiRoot)
-  // }
-
-  //submitted = false;
-
-
- // onSubmit(){ 
-    //console.log(JSON.stringify(this.feedback));
-   // console.log("GET all feedback called!");
-    // let url = `${this.apiRoot}`;
-    // let headers = new Headers();
-    // headers.append('Content-Type', 'application/json');
-    // let opts = new RequestOptions();
-    // opts.headers = headers;
-    // this.http.get(url, opts).subscribe(
-    //   res => console.log(res.json()),
-    //   msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
-    // );
-  //}
-  /*
-  onSubmit1(){
+*/
+  onSubmit(){
     console.log("Submit new Feedback!");
-    let url = `${this.apiRoot}`;
-    //let headers = new Headers();
-    //headers.append('Content-Type', 'application/json');
-    //let opts = new RequestOptions();
-    //opts.headers = headers;
-    this.http.post(url, this.feedback).subscribe(
-      res => console.log(res.json()),
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let opts = new RequestOptions();
+    opts.headers = headers;
+    this.http.post(this.apiRoot, this.feedback).subscribe(
+      res => {
+        this.feedbackSubmittedSinceServerStart = this.feedbackSubmittedSinceServerStart + 1;
+        console.log(res.json());
+      },
       msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
     );
   }
-
-  //get diagnostic() { return JSON.stringify(this.feedback); }
-
-  //constructor() { }
-
-  ngOnInit() {
+  
+  ngOnInit(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let opts = new RequestOptions();
+    opts.headers = headers;
+    this.http.get(this.apiRoot).subscribe(
+      res => { 
+        this.feedbackList = res.json(); 
+        this.feedbackSubmittedSinceServerStart = this.feedbackList.length;
+        console.log('Total Feedback registered: ' + this.feedbackSubmittedSinceServerStart)},
+      msg => console.error('Error: ${msg.status} ${msg.statusText}')
+    );
   }
-  */
   get currentFeedback() { return JSON.stringify(this.feedback); }
+  get totalFeedback() { 
+    if(this.feedbackList)
+      this.feedbackSubmitMsg = 'Total feedback registered till date: ' + this.feedbackSubmittedSinceServerStart; 
+    return this.feedbackSubmitMsg;
+  }
 
 }
